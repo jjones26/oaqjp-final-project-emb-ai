6ac14f9dd25cc3074b7be0d1e38bb3763e06e3fa
Jacob Jones <keneast24@gmail.com>
2026-04-30 20:09:20 -0400

    Enhance emotion detection with score extraction
    Extract emotion scores and determine the dominant emotion from the response.

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,8 +7,31 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     
     response = requests.post(url, json=myobj, headers=header)
-
-    # STEP 1: Convert the response text into a dictionary
     formatted_response = json.loads(response.text)
-    
-    return formatted_response
+
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+
+
+emotion_list = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(emotion_list, key=emotion_list.get)
+   
+    # temporary dictionary to test
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
